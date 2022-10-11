c18fd111b61bd8226aefe32002aca059f698cb7e
dgayet <dgayet@fi.uba.ar>
2022-10-11 02:24:22 -0300

    added script for adding commands with builder

---
+++ deploy_commands.js
@@ -0,0 +1,23 @@
+// deploy commands file
+var _a = require('discord.js'), REST = _a.REST, SlashCommandBuilder = _a.SlashCommandBuilder, Routes = _a.Routes;
+var _b = require('./config.json'), APP_ID = _b.APP_ID, SERVER_ID = _b.SERVER_ID, BOT_TOKEN = _b.BOT_TOKEN;
+// array with commands
+var commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with ping'),
+    new SlashCommandBuilder()
+        .setName('usd2ars')
+        .setDescription('Replies with equivalent ARS value of (USD) argument passed')
+        .addNumberOption(function (option) { return option.setName('usdval')
+        .setDescription('The USD value tu convert')
+        .setRequired(true); }),
+    new SlashCommandBuilder()
+        .setName('ars2usd')
+        .setDescription('Replies with equivalent USD value of (ARS) argument passed')
+        .addNumberOption(function (option) { return option.setName('arsval')
+        .setDescription('The ARS value tu convert')
+        .setRequired(true); }),
+]
+    .map(function (command) { return command.toJSON(); });
+var rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
+rest.put(Routes.applicationGuildCommands(APP_ID, SERVER_ID), { body: commands })
+    .then(function (data) { return console.log("Successfully registered ".concat(data.length, " application commands.")); })["catch"](console.error);
---
+++ deploy_commands.ts
@@ -0,0 +1,28 @@
+// deploy commands file
+
+const {REST, SlashCommandBuilder, Routes} = require('discord.js');
+const {APP_ID, SERVER_ID, BOT_TOKEN} = require('./config.json');
+
+// array with commands
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with ping'),
+	new SlashCommandBuilder()
+        .setName('usd2ars')
+        .setDescription('Replies with equivalent ARS value of (USD) argument passed')
+        .addNumberOption(option => option.setName('usdval')
+            .setDescription('The USD value tu convert')
+            .setRequired(true)),
+	new SlashCommandBuilder()
+        .setName('ars2usd')
+        .setDescription('Replies with equivalent USD value of (ARS) argument passed')
+        .addNumberOption(option => option.setName('arsval')
+            .setDescription('The ARS value tu convert')
+            .setRequired(true)),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(APP_ID, SERVER_ID), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error);
